a9d8ac5e7888eb972645e5b9a19c9063235eb2f3
Camilo Arguello <carguello@thefork.com>
2026-04-15 21:35:00 +0200

    refactor(transitions): removing non used import

--- Transition_Phase_Tailles/transition_phase.py
+++ Transition_Phase_Tailles/transition_phase.py
@@ -11,7 +11,6 @@ sys.path.insert(0, str(Path(__file__).parent.parent))
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-from numba import njit
 from utils.utils import (
     remplissage_aleatoire_reseau,
     calculate_magnetization,
