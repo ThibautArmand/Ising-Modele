40b757a0e70319605634c9b1ccf5ede936cf0b03
Camilo Arguello <carguello@thefork.com>
2026-05-01 10:56:55 +0200

    refactor(fot): cleaning & adding linespace

--- Effet champ magnétique extérieur/χ_L(H).py
+++ Effet champ magnétique extérieur/χ_L(H).py
@@ -12,8 +12,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from utils.utils import (
     remplissage_aleatoire_reseau,
-    calculate_H,
-    calculate_dE,
     MonteCarlo,
     format_time
 )
@@ -27,8 +25,8 @@ d = 2
 #champ magnétique extérieur
 amplitude_h=5
 taille_h=200
-h_up=np.arange(-amplitude_h,amplitude_h,2*amplitude_h/taille_h)
-h_down=np.arange(amplitude_h,-amplitude_h,-2*amplitude_h/taille_h)
+h_up=np.linspace(-amplitude_h,amplitude_h,taille_h)
+h_down=np.linspace(amplitude_h,-amplitude_h,taille_h)
 h=np.concatenate([h_up, h_down])
 
 T_s = [0.5,1.5,2.0,5]#température variable
@@ -59,19 +57,17 @@ for i, T in enumerate(T_s):
 
         fig , (ax1,ax2) = plt.subplots(1,2,figsize=(10, 10))
         ax1.plot(h_up,m_up,'k-',linewidth=0.65,label=f'h croissant: {h_up[0]:.2f} à {h_up[-1]:.2f}')
-        ax1.plot(h_down,m_down,'r-',linewidth=0.65,label=f'h décroissant: {h_up[0]:.2f} à {h_up[-1]:.2f}')
+        ax1.plot(h_down,m_down,'r-',linewidth=0.65,label=f'h décroissant: {h_down[0]:.2f} à {h_down[-1]:.2f}')
         ax1.set_xlim(-amplitude_h, amplitude_h)
         ax1.set_ylim(-1.1, 1.1)
         ax1.set_xlabel("h",loc='right')
         ax1.set_ylabel(r'$ m $',loc='top')
-        #ax1.set_subplot(111).legend(loc='upper center',ncol=2, bbox_to_anchor=(0.5, -0.15))
         ax1.set_title(f'Hystérésis de l\'aimantation \n réseau de taille L={L} - h={amplitude_h} à T={T}')
         ax1.set_box_aspect(1)
         
         ax2.plot(h_up,chi_num,'k-',linewidth=0.65,label=f'χ_L numérique (L={L})')
         ax2.plot(h_up,chi_th,'r--',linewidth=1.0,label=f'χ_L Binder (M_L={M_L:.3f})')
         ax2.set_xlim(-amplitude_h, amplitude_h)
-        #ax2.set_ylim(-1.1, 1.1)
         ax2.set_xlabel("h",loc='right')
         ax2.set_ylabel(r'$ χ_L(h) $',loc='top')
         ax2.set_title(f'χ_L(h) \n réseau de taille L={L} à T={T}')
