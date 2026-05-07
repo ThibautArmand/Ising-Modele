61b6b841b54de19f4d8270b75cc898ec3d5bc400
Camilo Arguello <carguello@thefork.com>
2026-05-07 21:00:55 +0200

    chore(utils): fixing function for magnetization

--- utils/utils.py
+++ utils/utils.py
@@ -161,9 +161,9 @@ def calculate_magnetization(Reseau):
     Returns
     -------
     M : float
-        Aimantation totale
+        Aimantation par spin
     """
-    return np.sum(Reseau)
+    return np.mean(Reseau)
 
 @njit
 def calculate_total_energy(Reseau, J=1.0, h=0.0):
@@ -244,11 +244,10 @@ def simulate_single_temperature(L, T, n_equilibration=1000, n_measurements=1000,
     e = E_mean / N
     
     # Aimantation par spin
-    m = M_mean_abs / N
+    m = M_mean_abs
 
     # Susceptibilité magnétique χ(T)
-    # chi = (N / T) * (M2_mean_signed - M_mean_signed**2) 
-    chi = (M2_mean_signed - M_mean_signed**2) / (N * T)
+    chi = (N / T) * (M2_mean_signed - M_mean_signed**2)
 
     # Chaleur spécifique  C(T)
     C = (1 / (T**2)) * (E2_mean - E_mean**2)
